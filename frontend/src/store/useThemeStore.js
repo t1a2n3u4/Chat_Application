3c3e2977e0b792b454811e8ccb43e39a459ccf4f
tanishka <tanishkamishra488@gmail.com>
2025-08-06 10:54:32 +0530

    Updated theme store and package files

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import { createWithEqualityFn } from "zustand/traditional";
 
 export const useThemeStore = create((set) => ({
   theme: localStorage.getItem("streamify-theme") || "coffee",
